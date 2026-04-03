bbe1e3a948a32738771f905e5d345c98e118aec2
MeetGoyal27 <165554078+MeetGoyal27@users.noreply.github.com>
2026-04-03 11:13:39 +0530

    Time: 31 ms (25.01%), Space: 47.5 MB (99.03%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,87 +1,69 @@
 class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        int largest = 0;
-        int[] ans = new int[matrix[0].length];
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                int val = matrix[i][j] - '0';
-                if(val == 0){
-                    ans[j] = 0;
-                }
-                else{
-                    ans[j] = ans[j] + val;
-                }
+    public int[] findNext(int[] arr){
+        Stack<Integer> st = new Stack<>();
+        int n = arr.length;
+        int[] ans = new int[n];
+        for(int i=n-1;i>=0;i--){
+            while(st.size()>0 && arr[i]<=arr[st.peek()]){
+                st.pop();
             }
-            int max = largestRectangleArea(ans);
-            if(max > largest){
-                largest = max;
+            if(st.isEmpty()){
+                ans[i] = n;
             }
-
+            else{
+                ans[i] = st.peek();
+            }
+            st.push(i);
         }
-        return largest;
-        
+        return ans;
+    }
+    public int[] findPrevious(int[] arr){
+        Stack<Integer> st = new Stack<>();
+        int n = arr.length;
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+            while(st.size()>0 && arr[i] <= arr[st.peek()]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i] = -1;
+            }
+            else{
+                ans[i] = st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
     }
     public int largestRectangleArea(int[] heights) {
-        final int n = heights.length;
+        int[] ps = findPrevious(heights);
+        int[] ns = findNext(heights);
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<heights.length;i++){
+            int h = heights[i];
+            int w = ns[i] - ps[i] - 1;
+            max = Math.max(max,h*w);
 
-        int[] leftSmall = firstSmallOnLeft(heights, n);
-        int[] rightSmall = firstSmallOnRight(heights, n);        
-
-        int ans = 0;
-        for(int i=0; i<n; i++) {
-            int leftIndex = leftSmall[i];
-            int rightIndex = rightSmall[i];
-
-            int width = rightIndex - leftIndex - 1;
-            int height = heights[i];
-
-            int area = width * height;
-            ans = Math.max(ans, area);
         }
-
-        return ans;        
+        return max;
     }
-    public int[] firstSmallOnRight(int[] heights, int n) {
-        int[] arr = new int[n];
-        Stack<Integer> st = new Stack<>();
-        
-
-        for(int i=n-1; i>=0; i--) {
-            while(st.size()>0 && heights[st.peek()] >= heights[i] ){
-                st.pop();
+    public int maximalRectangle(char[][] matrix) {
+        int max = Integer.MIN_VALUE;
+        int[] arr = new int[matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                int x = matrix[i][j] - '0';
+                if(x == 0){
+                    arr[j] = 0;
+                }
+                else{
+                    arr[j] = arr[j] + x;
+                }
             }
-            if(st.size() == 0){
-              arr[i] = n;
-            }
-            else{
-
-            arr[i] = st.peek();
-            }
-            st.push(i);
+            int maxArea = largestRectangleArea(arr);
+            max = Math.max(max,maxArea);
         }
-
-        return arr;
-
-    }
-    public int[] firstSmallOnLeft(int[] heights, int n) {
-        int[] arr = new int[n];
-        Stack<Integer> st = new Stack<>();
+        return max;
         
-
-        for(int i=0; i<n; i++){
-            while(st.size() > 0 && heights[st.peek()] >= heights[i] ){
-                st.pop();
-            }
-            if(st.size() == 0){
-                arr[i] = -1;
-            }
-            else{
-         arr[i] = st.peek();
-            }
-            
-            st.push(i);
-        }
-
-        return arr;
     }
 }
