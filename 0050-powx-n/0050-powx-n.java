85d40e8b4b418a9059e5a8d2c365de335c4ed23a
MeetGoyal27 <165554078+MeetGoyal27@users.noreply.github.com>
2026-04-13 17:05:57 +0530

    Time: 0 ms (100%), Space: 47 MB (97.98%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -4,22 +4,20 @@ class Solution {
         long nn = n;
         if(nn<0){
             nn = -1*nn;
-
         }
         while(nn>0){
-            if(nn%2 == 0){
-                x = x*x;
-                nn = nn/2;
+            if(nn%2 == 1){
+                nn = nn-1;
+                ans = ans*x;
             }
             else{
-                ans = ans*x;
-                nn = nn-1;
+                x = x*x;
+                nn = nn/2;
             }
         }
         if(n<0){
             return 1.0/ans;
         }
         return ans;
-
     }
 }
