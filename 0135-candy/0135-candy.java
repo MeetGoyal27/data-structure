c4dc87dd4d7b86fef80770b83915412b2acd18f5
MeetGoyal27 <165554078+MeetGoyal27@users.noreply.github.com>
2026-05-30 20:22:51 +0530

    Time: 3 ms (88.56%), Space: 45.6 MB (99.98%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] ans = new int[ratings.length];
+        Arrays.fill(ans,1);
+        for(int i=1;i<ratings.length;i++){
+            if(ratings[i] > ratings[i-1] && ans[i] <= ans[i-1]){
+                ans[i] = ans[i-1] + 1;
+            }
+        }
+        for(int i = ratings.length-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1] && ans[i] <= ans[i+1]){
+                ans[i] = ans[i+1] + 1;
+            }
+        }
+        int answer = 0;
+        for(int i=0;i<ans.length;i++){
+            answer += ans[i];
+        }
+        return answer;
+    }
+}
