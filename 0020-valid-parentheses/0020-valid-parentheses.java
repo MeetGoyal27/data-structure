7383b7008851ffc85e13261857d3fa0cde614629
MeetGoyal27 <165554078+MeetGoyal27@users.noreply.github.com>
2026-06-03 23:52:08 +0530

    Time: 4 ms (39.65%), Space: 42.8 MB (97.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,6 +3,9 @@ class Solution {
          if(s.length() == 0){
             return true;
         }
+        if(s.length() % 2 != 0){
+            return false;
+        }
         Stack<Character> st = new Stack<>();
          for (int i = 0; i < s.length(); i++) {
             char currentChar = s.charAt(i);
