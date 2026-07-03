28dc9ce912415f41f6416b426a2eaba25ea97e3f
MeetGoyal27 <165554078+MeetGoyal27@users.noreply.github.com>
2026-07-03 18:59:59 +0530

    Time: 0 ms (100%), Space: 46.7 MB (61.58%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,25 +1,24 @@
 class Solution {
-    public void dfs(int row,int col,int[][] image,boolean[][] vis,int color,int isColor){
-        if(row<0 || col<0 || row >= image.length || col >= image[0].length){
-            return;
-        }
-        if(image[row][col] != isColor){
+    public void helper(int row,int col,int[][] image,boolean[][] vis,int color,int isColor){
+        if(row<0 || col<0 || row>=image.length || col >= image[0].length){
             return;
         }
         if(vis[row][col] == true){
             return;
         }
+        if(image[row][col] != isColor){
+            return;
+        }
         vis[row][col] = true;
         image[row][col] = color;
-        dfs(row+1,col,image,vis,color,isColor);
-        dfs(row-1,col,image,vis,color,isColor);
-        dfs(row,col+1,image,vis,color,isColor);
-        dfs(row,col-1,image,vis,color,isColor);
-
+        helper(row+1,col,image,vis,color,isColor);
+        helper(row-1,col,image,vis,color,isColor);
+        helper(row,col+1,image,vis,color,isColor);
+        helper(row,col-1,image,vis,color,isColor);
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         boolean[][] vis = new boolean[image.length][image[0].length];
-        dfs(sr,sc,image,vis,color,image[sr][sc]);
+        helper(sr,sc,image,vis,color,image[sr][sc]);
         return image;
     }
 }
