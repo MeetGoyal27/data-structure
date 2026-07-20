baabd9159fae40ef158c4f8e8dc160f901a058d9
MeetGoyal27 <165554078+MeetGoyal27@users.noreply.github.com>
2026-07-20 14:00:03 +0530

    Time: 0 ms (100%), Space: 42.8 MB (37.59%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int recur(int idx,int[] nums,int[] dp){
+        if(idx == nums.length-1){
+            dp[idx] =  nums[idx];
+            return dp[idx];
+        }
+        if(idx == nums.length){
+            dp[idx] = 0;
+            return dp[idx];
+        }
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+        int pick = nums[idx] + recur(idx+2,nums,dp);
+        int noPick = recur(idx+1,nums,dp);
+        dp[idx] =  Math.max(pick,noPick);
+        return dp[idx];
+    }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return recur(0,nums,dp);
+    }
+}
