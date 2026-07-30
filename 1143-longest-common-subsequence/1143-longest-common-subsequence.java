bfb30f8162d0f81c4ea6389e4cfbdb725448cfe2
MeetGoyal27 <165554078+MeetGoyal27@users.noreply.github.com>
2026-07-30 18:23:31 +0530

    Time: 32 ms (9.39%), Space: 54 MB (52.59%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,21 +1,19 @@
 class Solution {
-    public int recur(int idx1,int idx2,String s1,String s2,int[][] dp){
-        if(idx1 == s1.length() || idx2 == s2.length()){
-            dp[idx1][idx2] = 0;
+    public int recur(int i,int j,String s1,String s2,int[][] dp){
+        if(i==s1.length() || j==s2.length()){
+            dp[i][j] = 0;
             return 0;
         }
-        if(dp[idx1][idx2] != -1){
-            return dp[idx1][idx2];
+        if(s1.charAt(i) == s2.charAt(j)){
+            dp[i][j] = 1+recur(i+1,j+1,s1,s2,dp);
+            return dp[i][j];
         }
-        if(s1.charAt(idx1) == s2.charAt(idx2)){
-            dp[idx1][idx2] = 1 + recur(idx1+1,idx2+1,s1,s2,dp);
-            return dp[idx1][idx2];
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
         else{
-            int first = recur(idx1,idx2+1,s1,s2,dp);
-            int second = recur(idx1+1,idx2,s1,s2,dp);
-            dp[idx1][idx2] = Math.max(first,second);
-            return dp[idx1][idx2];
+            dp[i][j] = Math.max(recur(i+1,j,s1,s2,dp),recur(i,j+1,s1,s2,dp));
+            return dp[i][j];
         }
     }
     public int longestCommonSubsequence(String text1, String text2) {
