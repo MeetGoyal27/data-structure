b212af743d02c8178bbd303e072be4c3d257e125
MeetGoyal27 <165554078+MeetGoyal27@users.noreply.github.com>
2026-08-11 19:36:24 +0530

    Time: 14 ms (70.72%), Space: 47.8 MB (33.17%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,21 +1,27 @@
 class Solution {
+    class Pair{
+        int val;
+        int freq;
+        public Pair(int val,int freq){
+            this.val=val;
+            this.freq=freq;
+        }
+    }
     public int[] topKFrequent(int[] nums, int k) {
         int[] ans = new int[k];
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int x : nums){
-            if(map.containsKey(x)){
-                map.put(x,map.get(x)+1);
-            }
-            else{
-                map.put(x,1);
-            }
-        }                
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
-        for(int key : map.keySet()){
-            pq.offer(key);
+            map.put(x,map.getOrDefault(x,0)+1);
         }
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Integer.compare(b.freq,a.freq));
+        for(int key : map.keySet()){
+            pq.offer(new Pair(key,map.get(key)));
+        }
+        int j=0;
         for(int i=0;i<k;i++){
-            ans[i] = pq.poll();
+            Pair p = pq.poll();
+            ans[j] = p.val;
+            j++;
         }
         return ans;
     }
