1237801616589e34ef71d2fe8e519c303d8705d4
MeetGoyal27 <165554078+MeetGoyal27@users.noreply.github.com>
2026-08-12 12:32:04 +0530

    Time: 7 ms (72.23%), Space: 50 MB (24.97%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs.length == 0){
+            return new ArrayList<>();
+        }
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String str : strs){
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String s = new String(arr);
+            if(map.containsKey(s)){
+                map.get(s).add(str);
+            }
+            else{
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(s,list);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+}
