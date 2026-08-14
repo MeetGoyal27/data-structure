3fadf7170712fd30df8e5a44269f1b4b2d6021c7
MeetGoyal27 <165554078+MeetGoyal27@users.noreply.github.com>
2026-08-15 01:08:10 +0530

    Time: 2 ms (98.26%), Space: 43.4 MB (41.49%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-         if(s.length() == 0){
-            return true;
-        }
-        if(s.length() % 2 != 0){
-            return false;
-        }
         Stack<Character> st = new Stack<>();
-         for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            if (currentChar == '(' || currentChar == '[' || currentChar == '{') {
-                st.push(currentChar);
-            } else {
-                if (st.isEmpty()) {
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+            else{
+                if(st.size() == 0){
                     return false;
                 }
-                char topChar = st.pop();
-                if ((currentChar == ')' && topChar != '(') ||
-                    (currentChar == ']' && topChar != '[') ||
-                    (currentChar == '}' && topChar != '{')) {
+                char c = st.pop();
+                if((ch==')' && c!='(') || (ch==']' && c!='[') || (ch=='}' && c!='{')){
                     return false;
                 }
             }
         }
-         return st.isEmpty();
+        return st.size() == 0;
     }
 }
