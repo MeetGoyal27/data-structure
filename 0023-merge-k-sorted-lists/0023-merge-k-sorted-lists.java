e53b0320a1a09e0bb96b31d0b31fac13fc732671
MeetGoyal27 <165554078+MeetGoyal27@users.noreply.github.com>
2026-08-24 23:38:29 +0530

    Time: 5 ms (40.77%), Space: 47 MB (13.96%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,28 +10,22 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        // Add all node values to the priority queue
-        for (ListNode temp : lists) {
-            while (temp != null) {
-                pq.add(temp.val); // Add the node's value to the priority queue
-                temp = temp.next;  // Move to the next node
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)->Integer.compare(a.val,b.val));
+        for(ListNode head : lists){
+            if(head != null){
+                pq.offer(head);
             }
         }
-
-        // Create a dummy node to simplify merging the linked list
-        ListNode temp = new ListNode();
-        ListNode point = temp; // Pointer to the head of the merged list
-
-        // Build the merged linked list from the priority queue
-        while (!pq.isEmpty()) {
-            ListNode newNode = new ListNode(pq.poll());  // Poll the smallest value from the priority queue
-            temp.next = newNode;  // Attach it to the result list
-            temp = temp.next;     // Move to the next node
+        while(pq.size()>0){
+            ListNode node = pq.poll();
+            curr.next = node;
+            curr = curr.next;
+            if(node.next != null){
+                pq.offer(node.next);
+            }
         }
-
-        return point.next;  // Return the merged list (skip the dummy node)
+        return dummy.next;
     }
 }
