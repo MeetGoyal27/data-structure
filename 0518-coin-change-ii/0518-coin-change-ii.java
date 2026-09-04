0879b0b4d4cf60d14084a105ca79ab53eb94c82f
MeetGoyal27 <165554078+MeetGoyal27@users.noreply.github.com>
2026-09-04 10:01:28 +0530

    Time: 39 ms (5.02%), Space: 56.9 MB (39.15%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,27 +1,21 @@
 class Solution {
     public int recur(int idx,int curr,int target,int[] arr,int[][] dp){
         if(curr == target){
-            dp[idx][curr] = 1;
             return 1;
         }
-        if(idx == arr.length){
-            dp[idx][curr] = 0;
-            return 0;
-        }
-        if(curr > target){
+        if(curr > target || idx == arr.length){
             return 0;
         }
         if(dp[idx][curr] != -1){
             return dp[idx][curr];
         }
-        int pick = recur(idx,curr+arr[idx],target,arr,dp);
-        int noPick = recur(idx+1,curr,target,arr,dp);
-        dp[idx][curr] = pick + noPick;
+        int t = recur(idx,curr+arr[idx],target,arr,dp);
+        int n = recur(idx+1,curr,target,arr,dp);
+        dp[idx][curr] = t+n;
         return dp[idx][curr];
     }
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
+        int[][] dp = new int[coins.length+1][amount+1];
         for(int[] row : dp){
             Arrays.fill(row,-1);
         }
